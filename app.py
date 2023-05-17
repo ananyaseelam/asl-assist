5bb369fc10cbc8994df39b61d3d715a08b8b656f
tkn24 <tkn24@cornell.edu>
2023-05-17 15:55:29 -0400

    added sentences

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from camerahands import run_fingerspelling 
-from detectwords import detect_words 
+from camerahands import run_fingerspelling
+from detectwords import detect_words
 
 # import camerahands
 # import detectwords
 
-def main (): 
-  print("WELCOME TO THE ASL ASSIST APPLICATION")
-  print("Please select one of these options by entering 1, 2, or 3: ")
-  print("(1) Detect Fingerspelling ASL: ")
-  print("(2) Detect ASL Words: ")
-  print("(3) Detect ASL Motions: ")
-  option = input()
 
-  if option == '1': 
-    run_fingerspelling()
-  elif option == '2': 
-    print('here')
-    detect_words()
+def main():
+    print("WELCOME TO THE ASL ASSIST APPLICATION")
+    print("Please select one of these options by typing 1 or 2 and hitting enter: ")
+    print("(1) Detect Fingerspelling ASL: ")
+    print("(2) Detect ASL Words: ")
+    option = input()
 
+    if option == '1':
+        print("Please wait while we load the video camera...")
+        run_fingerspelling()
+    elif option == '2':
+        print("Please wait while we load the video camera...")
+        detect_words()
 
 
 main()
